c4b6ce6aa5f0bd06ae4d40b2dfd4f680c343428f
Andy Patari <andy.patari@gmail.com>
2023-08-17 21:35:55 -0400

    add task list and delete buttons

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList( { tasks, setTasks }) {
+
+  const tasksToDisplay = tasks.slice()
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksToDisplay.map((task, index) => 
+        <Task 
+          key={uuid()} 
+          index={index}
+          text={task.text} 
+          category={task.category} 
+          tasks={tasks} 
+          setTasks={setTasks} 
+        />
+      )}
+    
+
+      
     </div>
   );
 }
